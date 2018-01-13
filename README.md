27e069953b909f9ae59b7894b64bb60ac8675bd1
nginnever <ginneversource@gmail.com>
2018-01-12 16:57:25 -0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # general-state-channels
 A set of ethereum contracts to abstract the management of authority contracts that may fit many use cases
+
---
+++ contracts/ChannelManager.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.18;
+
+contract ChannelManager {
+
+    struct Participant
+    {
+
+    }
+
+    struct Channel
+    {
+
+    }
+
+    mapping(address => Participant) participants;
+
+    function openChannel() public payable {
+
+    }
+
+    function closeChannel() public {
+        
+    }
+}
