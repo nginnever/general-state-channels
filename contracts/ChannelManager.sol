9068aa26bb7ad673aa698be411b347263d0d9e5c
nginnever <ginneversource@gmail.com>
2018-01-12 21:37:02 -0800

    signing state

--- contracts/ChannelManager.sol
+++ contracts/ChannelManager.sol
@@ -4,47 +4,80 @@ import "./JudgeInterface.sol";
 
 contract ChannelManager {
 
-    struct Participant
-    {
-        uint256 balance;
-        uint256 channelId;
-    }
-
     struct Channel
     {
-        address a;
-        address b;
+        address partyA;
+        address partyB;
         uint256 bond;
+        uint256 bonded;
         JudgeInterface judge;
 
         uint settlementPeriod;
+        bool open;
+        bool settling;
     }
 
     mapping(address => Participant) participants;
-    mapping(uint256 => Channel) channels;
+    mapping(bytes32 => Channel) channels;
 
-    uint256 public numChannels;
+    uint256 public numChannels = 0;
 
-    function openChannel(address _judge) public payable {
+    event ChannelCreated(bytes32 channelId, address indexed partyA, address indexed partyB);
+
+    function openChannel(address _partyB, uint _duration, uint _settlementPeriod, address _judge) public payable {
         JudgeInterface candidateContract = JudgeInterface(_judge);
 
         // NOTE: verify that a contract is what we expect - https://github.com/Lunyr/crowdsale-contracts/blob/cfadd15986c30521d8ba7d5b6f57b4fefcc7ac38/contracts/LunyrToken.sol#L117
         require(candidateContract.isJudge());
+        require(_partyB != 0x0);
 
-        Channel memory _channel = Channel(msg.sender, 0x0, msg.value, candidateContract, 0);
+
+        Channel memory _channel = Channel(
+            msg.sender, _partyB, msg.value, msg.value, candidateContract, _settlementPeriod, false, false);
+
         numChannels++;
-        channels[numChannels] = _channel;
+        var _id = keccak256(now + numChannels);
+        channels[_id] = _channel;
 
 
-        // Set the new contract address
+        ChannelCreated(_id, msg.sender, _partyB);
     }
 
-    function closeChannel() public {
+    function joinChannel(bytes32 _id) public payable{
+        require(channels[_id].partyB == msg.sender);
+        require(msg.value == channels[_id].bond);
+        channels[_id].open == true;
+        channels[_id].bonded += msg.value;
+    }
+
+    function closeChannel(bytes32 _id) public {
+        require(msg.sender == channels[_id].partyA || msg.sender == channels[_id].partyB);
 
     }
 
-    function exerciseJudge(uint256 _id, string _method, uint256 _value, bytes _data) public {
-        require(channels[_id].judge.call(bytes4(bytes32(sha3(_method))), msg.sender, _value, this, _data));
+    function exerciseJudge(bytes32 _id, string _method, uint8 v, bytes32 r, bytes32 s, uint256 _data) public {
+        uint256 _bonded = channels[_id].bonded;
+        channels[_id].bonded = 0;
+
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 h = keccak256(_data);
+        bytes32 prefixedHash = keccak256(prefix, h);
+        address challenged = ecrecover(prefixedHash, v, r, s);
+
+        require(challenged == channels[_id].partyA || challenged == channels[_id].partyB);
+        // assert that the state update failed the judge run
+        require(!channels[_id].judge.call(bytes4(bytes32(sha3(_method))), _data));
+        // punish the violator and close the channel
+        msg.sender.send(_bonded / 2);
+
+        // return bond to non-violator
+        if(challenged == channels[_id].partyA) {
+            channels[_id].partyB.send(_bonded);
+        } else {
+            channels[_id].partyA.send(_bonded);
+        }
+
+        delete channels[_id];
     }
 
     function challengeClose() public {
--- test/test.js
+++ test/test.js
@@ -3,21 +3,28 @@
 const utils = require('./helpers/utils')
 
 const ChannelManager = artifacts.require("./ChannelManager.sol")
-const Judge = artifacts.require("./Judge.sol")
+const Judge = artifacts.require("./JudgeAlways420.sol")
 
 let cm
 let jg
+
+let event_args
 
 contract('ChannelManager', function(accounts) {
   it("ChannelManager deployed", async function() {
     cm = await ChannelManager.new()
     jg = await Judge.new()
 
-    await cm.openChannel(jg.address)
+    let res = await cm.openChannel(accounts[1], 1337, 1337, jg.address)
     let numChan = await cm.numChannels()
     console.log('Channels open: ' + numChan.toNumber())
+    event_args = res.logs[0].args
 
-    await cm.exerciseJudge(1, 'testVar(bytes)', 0, 'test')
+    let channelId = event_args.channelId
+
+    await cm.exerciseJudge(channelId, 'testVar(uint256)', 420)
+    let data = await jg.data()
+    console.log('Contract data after proxy call: ' + data.toNumber())
   })
 
 })
